b43e25c011cf75b5c52d30411f6b0212f80a9f30
jonathan03ant <jonathan03ant@gmail.com>
2025-08-17 19:38:57 -0400

    [lsp-config.lua]: new lsp config with member compl

--- neovim/lua/plugins/lsp-config.lua
+++ neovim/lua/plugins/lsp-config.lua
@@ -1,44 +1,59 @@
 return {
-    {
-        -- this will be the mason server
-        -- run :Mason to see its config servers
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+      "hrsh7th/nvim-cmp",
+      "hrsh7th/cmp-nvim-lsp",
+      "nvimdev/lspsaga.nvim",
+      "ray-x/lsp_signature.nvim",
+      "folke/neodev.nvim",
     },
+    config = function()
+      -- Mason setup
+      require("mason").setup()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "bashls",
+          "ast_grep",
+          "clangd",
+          "harper_ls",
+          "cmake",
+          "dockerls",
+          "jsonls",
+          "pylysp",
+          "sqlls",
+        },
+      })
 
-    {
-        -- ESUNRE INSTALLED THIS LSP server languages
-        -- this makes sure individual language servers are installed 
-        -- if we want to add ls for a new language we can add it here 
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = {
-                    "lua_ls", --server for lua language
-                    "bashls",
-                    "ast_grep",
-                    "clangd",
-                    "harper_ls",
-                    "cmake",
-                    "dockerls",
-                    "jsonls",
-                    "pylsp",
-                    "sqlls",
-                }
-            })
-        end
-     },
-    {
-        -- Hooking nvim with lsp servers
-        "neovim/nvim-lspconfig",
-        config = function()
-            local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({})
-            
-        end
-    },
+      -- LSP and UI setup
+      local lspconfig = require("lspconfig")
+      local cmp_lsp = require("cmp_nvim_lsp")
+      local capabilities = cmp_lsp.default_capabilities()
+
+      require("neodev").setup()
+      require("lspsaga").setup()
+
+      local servers = {
+        "lua_ls", "bashls", "clangd", "cmake", "dockerls",
+        "jsonls", "pylysp", "sqlls",
+      }
+
+      for _, server in ipairs(servers) do
+        lspconfig[server].setup({
+          capabilities = capabilities,
+          on_attach = function(client, bufnr)
+            require("lsp_signature").on_attach({}, bufnr)
+            local buf = vim.lsp.buf
+            vim.keymap.set("n", "K", buf.hover, { buffer = bufnr })
+            vim.keymap.set("n", "gd", buf.definition, { buffer = bufnr })
+            vim.keymap.set("n", "gi", buf.implementation, { buffer = bufnr })
+            vim.keymap.set("n", "<leader>ca", buf.code_action, { buffer = bufnr })
+          end,
+        })
+      end
+    end,
+  },
 }
-
-
